f7da5f8dca3f911e85511caad320adc0797d595a
hoge8086 <hoge8086@gmail.com>
2021-03-27 02:34:21 +0900

    テストプロジェクトを追加

---
+++ ResourceStringCheckerTests/ResourceCheckerTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ResourceStringChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceStringChecker.Tests
+{
+    public class MockResourceFileReader : IResourceFileReader
+    {
+        public MockResourceFileReader() { }
+        public string Read(ResourceFile resource, string resourceId) { return "a"; }
+
+    }
+    public class MockSpecDocMetaInfoRepository : ISpecDocMetaInfoRepository
+    {
+        public SpecDocMetaInfo GetMetaInfo(string specName)
+        {
+            return new SpecDocMetaInfo()
+            {
+                SpecDocPath = "test.xlsx",
+                Sheets = new List<SpecDocSheet>()
+                {
+                    new SpecDocSheet()
+                    {
+                        TableHeaders = new List<TableHeader>()
+                        {
+                            new TableHeader()
+                            {
+                                LanguageColumns = new List<LanguageColumn>()
+                                {
+                                    new LanguageColumn() { Column = "A", Language = Language.English },
+                                    new LanguageColumn() { Column = "B", Language = Language.Japanease},
+                                },
+                                ResourceIdColumn = "C",
+                            },
+                        },
+                        ResourceFiles = new List<ResourceFile> {
+                            new ResourceFile { Language = Language.Japanease , FilePath="resource_jp.txt", Encoding="shift-jis"},
+                            new ResourceFile { Language = Language.English, FilePath="resource_en.txt", Encoding="shift-jis"},
+                        },
+                        SheetName = "sheet",
+                    },
+                },
+            };
+        }
+    }
+
+    [TestClass()]
+    public class ResourceCheckerTests
+    {
+        [TestMethod()]
+        public void CheckTest()
+        {
+            var checker = new ResourceChecker(new MockResourceFileReader(), new MockSpecDocMetaInfoRepository());
+            var results = checker.Check(new List<CheckTarget>() {
+                new CheckTarget() { SpecName="spec", SheetName="sheet", CheckRows= new List<int> { 1, 2 } } }
+            );
+            //Assert.Fail();
+        }
+    }
+}
